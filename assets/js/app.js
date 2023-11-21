606ce51f121f682ab8ebc983b42a116c0a15e7fd
Mohammad Keshavarz <willimasbron13@gmail.com>
2023-11-21 10:58:39 +0330

    update index.html compeleted

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,166 +1,169 @@
 // const Layouts = document.querySelectorAll('.layout');
-const CategoryLinks = document.querySelectorAll('.category');
-const CategoriesContainer = document.querySelector('.categories');
-const CategoriesContainer__h2 = CategoriesContainer.querySelector('.h2');
+const CategoryLinks = document.querySelectorAll(".category");
+const CategoriesContainer = document.querySelector(".categories");
+const CategoriesContainer__h2 = CategoriesContainer.querySelector(".h2");
 
-const PriceFilter = document.querySelector('.price-filter')
-const PriceFilter__h2 = PriceFilter.querySelector('.h2')
+const PriceFilter = document.querySelector(".price-filter");
+const PriceFilter__h2 = PriceFilter.querySelector(".h2");
 
+const PriceFilterRange = document.querySelector(".price-range");
+const PriceFilterRange__h2 = PriceFilterRange.querySelector(".h2");
 
-const PriceFilterRange = document.querySelector('.price-range')
-const PriceFilterRange__h2 = PriceFilterRange.querySelector('.h2')
+const ShowingFilters = document.querySelectorAll(".showing-filter");
+const FilterList = document.querySelectorAll(".filter-lists");
+const inputSlider = document.querySelector("input[type=range]");
+const sliderValue = document.querySelector(".sliderValue");
 
-const ShowingFilters = document.querySelectorAll('.showing-filter');
-const FilterList = document.querySelectorAll('.filter-lists');
-const inputSlider = document.querySelector('input[type=range]');
-const sliderValue = document.querySelector('.sliderValue');
+const appliedFilter = document.querySelector("#applied-filters");
+const appliedFilter__h2 = appliedFilter.querySelector(".h2");
 
-const appliedFilter = document.querySelector('#applied-filters');
-const appliedFilter__h2 = appliedFilter.querySelector('.h2');
-
-const appliedFilter__iconDown = appliedFilter.querySelector('#iconDown');
+const appliedFilter__iconDown = appliedFilter.querySelector("#iconDown");
 // filters
-appliedFilter__h2.addEventListener('click', () => {
-	appliedFilter__iconDown.classList.toggle('rotate');
-	appliedFilter.querySelector('.filters').classList.toggle('hidden')
+appliedFilter__iconDown.addEventListener("click", () => {
+	appliedFilter__iconDown.classList.toggle("rotate");
+	appliedFilter.querySelector(".filters").classList.toggle("hidden");
 });
 
-appliedFilter.querySelectorAll('.selected-filter').forEach(filter => {
-	const icon = filter.querySelector('.remove-filter-icon');
+appliedFilter.querySelectorAll(".selected-filter").forEach((filter) => {
+	const icon = filter.querySelector(".remove-filter-icon");
 
-	icon.addEventListener('click', () => {
-		filter.classList.add('remove');
+	icon.addEventListener("click", () => {
+		filter.classList.add("remove");
 		setTimeout(() => {
-			filter.remove()
-		}, 300)
-	})
-})
-
-CategoriesContainer__h2.addEventListener('click', () => {
-	CategoriesContainer.querySelector('#iconDown').classList.toggle('rotate');
-	CategoriesContainer.querySelector('.categorie-links').classList.toggle('hidden')
-})
-
-PriceFilter__h2.addEventListener('click', () => {
-	PriceFilter.querySelector('#iconDown').classList.toggle('rotate');
-	PriceFilter.querySelector('.prices__container').classList.toggle('hidden')
-
-})
-
-PriceFilterRange__h2.addEventListener('click', () => {
-	PriceFilterRange.querySelector('#iconDown').classList.toggle('rotate');
-	PriceFilterRange.querySelector('.price-range-container').classList.toggle('hidden');
-
-})
-
-const square = document.querySelector('#square');
-const line = document.querySelector('#line');
-const squareContainer = document.getElementById('square_layout');
-const lineContainer = document.getElementById('line_layout');
-
-square.addEventListener('click', () => {
-	lineContainer.style.display = 'none';
-	squareContainer.style.display = 'block';
-
-	line.classList.remove('selected');
-	square.classList.add('selected');
+			filter.remove();
+		}, 300);
+	});
 });
 
-line.addEventListener('click', () => {
-	lineContainer.style.display = 'block';
-	squareContainer.style.display = 'none';
+CategoriesContainer.querySelector("#iconDown").addEventListener("click", () => {
+	CategoriesContainer.querySelector("#iconDown").classList.toggle("rotate");
+	CategoriesContainer.querySelector(".categorie-links").classList.toggle(
+		"hidden"
+	);
+});
 
-	square.classList.remove('selected');
-	line.classList.add('selected');
+PriceFilter.querySelector("#iconDown").addEventListener("click", () => {
+	PriceFilter.querySelector("#iconDown").classList.toggle("rotate");
+	PriceFilter.querySelector(".prices__container").classList.toggle("hidden");
+});
+
+PriceFilterRange.querySelector("#iconDown").addEventListener("click", () => {
+	PriceFilterRange.querySelector("#iconDown").classList.toggle("rotate");
+	PriceFilterRange.querySelector(".price-range-container").classList.toggle(
+		"hidden"
+	);
+});
+
+const square = document.querySelector("#square");
+const line = document.querySelector("#line");
+const squareContainer = document.getElementById("square_layout");
+const lineContainer = document.getElementById("line_layout");
+
+square.addEventListener("click", () => {
+	lineContainer.style.display = "none";
+	squareContainer.style.display = "block";
+
+	line.classList.remove("selected");
+	square.classList.add("selected");
+});
+
+line.addEventListener("click", () => {
+	lineContainer.style.display = "block";
+	squareContainer.style.display = "none";
+
+	square.classList.remove("selected");
+	line.classList.add("selected");
 });
 
 CategoryLinks.forEach((link) => {
-	const submenu = link.querySelector('#digital-product-submenu');
-	const icon = (link.querySelector('#digital-product-icon').querySelector('.yoz-arrow-up-2'));
+	const submenu = link.querySelector("#digital-product-submenu");
+	const icon = link
+		.querySelector("#digital-product-icon")
+		.querySelector(".yoz-arrow-up-2");
 
-	link.addEventListener('click', () => {
-		submenu.classList.toggle('open');
-		icon.classList.toggle('rotate');
+	link.addEventListener("click", () => {
+		submenu.classList.toggle("open");
+		icon.classList.toggle("rotate");
 	});
 });
 
 ShowingFilters.forEach((Filter) => {
-	const toggle_btn = Filter.querySelector('label');
-	console.log(toggle_btn)
+	const toggle_btn = Filter.querySelector("label");
+	console.log(toggle_btn);
 	var toggleValue = false; //* by default toggle button in false
 
-	toggle_btn.addEventListener('click', (event) => {
+	toggle_btn.addEventListener("click", (event) => {
 		event.preventDefault();
-		console.log(toggle_btn.offsetLeft)
+		console.log(toggle_btn.offsetLeft);
 		if (!toggleValue) {
-			toggle_btn.style.setProperty('--afterLeft', '1.3em');
-			toggle_btn.style.setProperty('--beforeBG', '#edeffe');
-			toggle_btn.style.setProperty('--afterBg', '#4b64f2');
+			toggle_btn.style.setProperty("--afterLeft", "1.3em");
+			toggle_btn.style.setProperty("--beforeBG", "#edeffe");
+			toggle_btn.style.setProperty("--afterBg", "#4b64f2");
 
 			toggleValue = !toggleValue;
-			console.log('availble Product: ' + toggleValue);
+			console.log("availble Product: " + toggleValue);
 		} else {
-			toggle_btn.style.setProperty('--afterLeft', '.1em');
-			toggle_btn.style.setProperty('--beforeBG', '#efefef');
-			toggle_btn.style.setProperty('--afterBg', '#929292');
+			toggle_btn.style.setProperty("--afterLeft", ".1em");
+			toggle_btn.style.setProperty("--beforeBG", "#efefef");
+			toggle_btn.style.setProperty("--afterBg", "#929292");
 
 			toggleValue = !toggleValue;
-			console.log('availble Product: ' + toggleValue);
+			console.log("availble Product: " + toggleValue);
 		}
 	});
 });
 
 FilterList.forEach((filterlist) => {
-	const FilterTitles = filterlist.querySelectorAll('.title');
-	const ItemsList = filterlist.querySelectorAll('.items-list');
-	const selectedItems = filterlist.querySelectorAll('.selected-item');
+	const FilterTitles = filterlist.querySelectorAll(".title");
+	const ItemsList = filterlist.querySelectorAll(".items-list");
+	const selectedItems = filterlist.querySelectorAll(".selected-item");
 
 	FilterTitles.forEach((title) => {
-		const icons = title.querySelectorAll('#icon');
+		const icons = title.querySelectorAll("#icon");
 
 		icons.forEach((icon) => {
-			title.addEventListener('click', () => {
-				icon.classList.toggle('rotate');
+			title.addEventListener("click", () => {
+				icon.classList.toggle("rotate");
 				ItemsList.forEach((Itemlist) => {
-					Itemlist.classList.toggle('hidden');
+					Itemlist.classList.toggle("hidden");
 				});
 			});
 		});
 	});
 
 	selectedItems.forEach((selectedItem) => {
-		selectedItem.addEventListener('click', () => {
-			const brandColorName = selectedItem.querySelector('.item-selected');
-			brandColorName.classList.toggle('select');
+		selectedItem.addEventListener("click", () => {
+			const brandColorName = selectedItem.querySelector(".item-selected");
+			brandColorName.classList.toggle("select");
 		});
 	});
 });
 
-const showMoreBtns = document.querySelectorAll('.showMoreButton');
-const showLessBtns = document.querySelectorAll('.showLessButton');
-const secondGroupOfItems = document.querySelectorAll('.second-items-group');
+const showMoreBtns = document.querySelectorAll(".showMoreButton");
+const showLessBtns = document.querySelectorAll(".showLessButton");
+const secondGroupOfItems = document.querySelectorAll(".second-items-group");
 
 function showMoreOfFirstGroup() {
-	showMoreBtns[0].style.display = 'none';
-	secondGroupOfItems[0].style.display = 'flex';
+	showMoreBtns[0].style.display = "none";
+	secondGroupOfItems[0].style.display = "flex";
 }
 function showMoreOfSecondGroup() {
-	showMoreBtns[1].style.display = 'none';
-	secondGroupOfItems[1].style.display = 'flex';
+	showMoreBtns[1].style.display = "none";
+	secondGroupOfItems[1].style.display = "flex";
 }
 
 function showLessOfFirstGroup() {
-	showMoreBtns[0].style.display = 'block';
-	secondGroupOfItems[0].style.display = 'none';
+	showMoreBtns[0].style.display = "block";
+	secondGroupOfItems[0].style.display = "none";
 }
 
 function showLessOfSecondGroup() {
-	showMoreBtns[1].style.display = 'block';
-	secondGroupOfItems[1].style.display = 'none';
+	showMoreBtns[1].style.display = "block";
+	secondGroupOfItems[1].style.display = "none";
 }
 
 const inputNumberOfPriceFilter = document.querySelectorAll(
-	'.price-filter input[type=number]'
+	".price-filter input[type=number]"
 );
 
 inputNumberOfPriceFilter.forEach((input) => {
@@ -174,28 +177,28 @@ inputNumberOfPriceFilter.forEach((input) => {
 inputSlider.oninput = () => {
 	let value = inputSlider.value;
 
-	inputSlider.style.setProperty('--width-range', value+"%")
+	inputSlider.style.setProperty("--width-range", value + "%");
 
 	sliderValue.textContent = !(value * 10 === 1000)
-		? value * 10 + 'هزار'
-		: 1 + 'میلیون';
+		? value * 10 + "هزار"
+		: 1 + "میلیون";
 	switch (value) {
 		case 100:
-			sliderValue.style.right = value - 15 + '%';
+			sliderValue.style.right = value - 15 + "%";
 			break;
 		case 0:
-			sliderValue.style.right = '5%';
+			sliderValue.style.right = "5%";
 			break;
 	}
-	sliderValue.style.right = value + '%';
-	sliderValue.classList.add('show');
+	sliderValue.style.right = value + "%";
+	sliderValue.classList.add("show");
 };
 
 inputSlider.onblur = () => {
-	sliderValue.classList.remove('show');
+	sliderValue.classList.remove("show");
 };
 
-var countDownTime = new Date('Nov 12 2023 14:00:00').getTime();
+var countDownTime = new Date("Nov 12 2023 14:00:00").getTime();
 
 var x = setInterval(() => {
 	var now = new Date().getTime();
@@ -208,14 +211,14 @@ var x = setInterval(() => {
 	var minutes = Math.floor((distance % (100 * 60 * 60)) / (1000 * 60));
 	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-	document.getElementById('hours').innerHTML = Math.floor(hours % 24) / 10;
-	document.getElementById('minutes').innerHTML = minutes;
-	document.getElementById('seconds').innerHTML = seconds;
+	document.getElementById("hours").innerHTML = Math.floor(hours % 24) / 10;
+	document.getElementById("minutes").innerHTML = minutes;
+	document.getElementById("seconds").innerHTML = seconds;
 
 	if (distance <= 0) {
 		clearInterval(x);
-		document.getElementById('hours').innerHTML = '00';
-		document.getElementById('minutes').innerHTML = '00';
-		document.getElementById('seconds').innerHTML = '00';
+		document.getElementById("hours").innerHTML = "00";
+		document.getElementById("minutes").innerHTML = "00";
+		document.getElementById("seconds").innerHTML = "00";
 	}
 }, 1000);
